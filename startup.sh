8240948e03368e0f0da2dbae16f3d74445935d53
intlabs <petebirley@gmail.com>
2014-06-06 03:16:51 +0100

    ...

--- startup.sh
+++ startup.sh
@@ -13,4 +13,4 @@ apt-get install -y fuse sshfs
 
 #Mount dropbox
 mkdir -p ~/.ssh && ssh -p 222 -o 'StrictHostKeyChecking no' -o "BatchMode yes" 172.16.0.147 cat /etc/ssh/ssh_host_dsa_key.pub >>~/.ssh/known_hosts
-mkdir -p /usr/share/nginx/html/home/admin && echo acoman | sshfs -p 222 root@172.16.0.147:/mnt/dropbox /usr/share/nginx/html/home/admin -o allow_root -o password_stdin
+mkdir -p /usr/share/nginx/html/home/admin/dropbox && echo acoman | sshfs -p 222 root@172.16.0.147:/mnt/dropbox /usr/share/nginx/html/home/admin/dropbox -o allow_root -o password_stdin
